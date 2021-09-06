b0bf305490054ea0d677cffea45c091aedc108e2
Ekaterina Ivanova <eivanova@cs-cart.com>
2021-09-06 15:14:20 +0400

    after 2nd step

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .parse(process.argv);
