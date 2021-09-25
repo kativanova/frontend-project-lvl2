f83b626145d7c8de22ce10af9a053afe3b088f7b
Ekaterina Ivanova <eivanova@cs-cart.com>
2021-09-25 13:18:51 +0400

    adds stylish to cli programm

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,9 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    gendiff(filepath1, filepath2);
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
   })
   .parse();
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,24 @@
 import * as fs from 'fs';
-import _ from 'lodash';
 import path from 'path';
 import parce from './parsers.js';
 import diffObj from './diffObject.js';
 import stylish from './formatters.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format) => {
   try {
     const file1 = fs.readFileSync(filepath1, 'utf8');
     const file2 = fs.readFileSync(filepath2, 'utf8');
 
     const obj1 = parce(file1, path.extname(filepath1));
     const obj2 = parce(file2, path.extname(filepath2));
-    return stylish(diffObj(obj1, obj2));
+
+    const diff = diffObj(obj1, obj2);
+
+    switch (format) {
+      case 'stylish':
+        return stylish(diff);
+      default:
+    }
   } catch (err) {
     console.log(err);
   }
